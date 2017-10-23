85d282a707dee38c67ebdb885ded1c1cd3221161
wangql <wangql@mail.prpiano.com>
2017-10-23 20:27:28 +0800

    添加图片资源，新建配置文件setting.php对图片的路径进行配置，模型读取器的巧妙应用

--- application/api/model/Image.php
+++ application/api/model/Image.php
@@ -7,4 +7,14 @@ use think\Model;
 class Image extends Model
 {
     protected $visible = ['url'];
+
+    public function getUrlAttr($value, $data)
+    {
+        $finalUrl = $value;
+        if ($data['from'] == 1) {
+            $finalUrl = config('setting.img_prefix').$finalUrl;
+        }
+
+        return $finalUrl;
+    }
 }
---
+++ application/extra/setting.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 随缘
+ * Date: 2017/10/23
+ * Time: 20:22
+ */
+
+return [
+    'img_prefix' => 'qiyue.com/images'
+];
