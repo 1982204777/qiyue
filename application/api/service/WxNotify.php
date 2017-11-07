cba9b4970a2cbd5facd0772635aa1d73aa1f49e4
wangql <wql123@prpianoapp.com>
2017-11-07 19:58:33 +0800

    支付回调通知处理

---
+++ application/api/service/WxNotify.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 随缘
+ * Date: 2017/11/7
+ * Time: 19:24
+ */
+
+namespace app\api\service;
+
+use app\api\model\Product;
+use app\lib\enum\OrderStatusEnum;
+use think\Exception;
+use think\Loader;
+use app\api\model\Order as OrderModel;
+use app\api\service\Order as OrderService;
+use think\Log;
+
+Loader::import('WxPay.WxPay', EXTEND_PATH, '.Api.php');
+
+class WxNotify extends \WxPayNotify
+{
+    public function NotifyProcess($data, &$msg)
+    {
+        if ($data['result_code'] == 'SUCCESS') {
+            //订单号
+            $orderNo = $data['out_trade_no'];
+            try {
+                $order = OrderModel::where('order_no', '=', $orderNo)->find();
+                //只处理状态为未支付的
+                if ($order->status == 1) {
+                    $service = new OrderService();
+                    $stockStatus = $service->checkOrderStock($order->id);
+                    if ($stockStatus['is_pass']) {
+                        $this->updateOrderStatus($order->id, true);
+                        $this->reduceStock($stockStatus);
+                    } else {
+                        $this->updateOrderStatus($order->id, false);
+                    }
+                }
+
+                return true;
+            } catch (Exception $e) {
+                Log::error($e->getMessage());
+
+                return false;
+            }
+        } else {
+            //如果还是返回false的话，微信还会继续发失败的消息
+            //这里的true 和 false 是控制微信是否继续向服务器发回调信息
+            return true;
+        }
+    }
+
+    //修改订单状态 已支付
+    private function updateOrderStatus($orderID, $success)
+    {
+        $status = $success ? OrderStatusEnum::PAID : OrderStatusEnum::PAID_BUT_OUT_OF;
+        OrderModel::where('id', '=', $orderID)->update([
+            'status' => $status
+        ]);
+    }
+
+    //减库存量
+    private function reduceStock($stockStatus)
+    {
+        foreach ($stockStatus['pStatusArray'] as $singlePStatus) {
+//            $singlePStatus['count']
+            Product::where('id', '=', $singlePStatus['id'])
+                ->setDec('stock', $singlePStatus['count']);
+
+        }
+    }
+}
