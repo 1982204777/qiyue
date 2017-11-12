70ac524726b573e880357e43612c26d88d8f6875
wangql <1982204777@qq.com>
2017-11-12 18:54:52 +0800

    新增获取用户地址信息接口

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -10,6 +10,7 @@ namespace app\api\controller\v1;
 
 
 use app\api\model\User as UserModel;
+use app\api\model\UserAddress;
 use app\api\service\Token as TokenService;
 use app\api\validate\AddressNew;
 use app\lib\enum\ScopeEnum;
@@ -22,7 +23,7 @@ use think\Controller;
 class Address extends BaseController
 {
     protected $beforeActionList = [
-        'checkPrimaryScope' => ['only' => 'createOrUpdateAddress']
+        'checkPrimaryScope' => ['only' => 'createOrUpdateAddress,getUserAddress']
     ];
 
     public function createOrUpdateAddress()
@@ -50,5 +51,20 @@ class Address extends BaseController
         }
 
         return json(new SuccessMessage(), 201);
+    }
+
+    public function getUserAddress()
+    {
+        $uid = TokenService::getCurrentUID();
+        $userAddress = UserAddress::where('user_id', $uid)->find();
+
+        if (!$userAddress) {
+            throw new UserException([
+                'msg' => '用户地址不存在',
+                'errorCode' => 60001
+            ]);
+        }
+
+        return $userAddress;
     }
 }
--- application/route.php
+++ application/route.php
@@ -38,6 +38,7 @@ Route::post('api/:version/token/user', 'api/:version.Token/getToken');
 Route::post('api/:version/token/verify', 'api/:version.Token/verifyToken');
 
 Route::post('api/:version/address', 'api/:version.Address/createOrUpdateAddress');
+Route::get('api/:version/address', 'api/:version.Address/getUserAddress');
 
 Route::post('api/:version/order', 'api/:version.Order/placeOrder');
 Route::get('api/:version/order/by_user', 'api/:version.Order/getSummaryByUser');
